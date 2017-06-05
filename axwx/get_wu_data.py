4bddf7c0e8cc3d42709eed0925d8c6a35063d1a6
Rex Thompson <RexS.Thompson@gmail.com>
2017-06-04 17:50:49 -0700

    functionalized get_wu_data

--- axwx/get_wu_data.py
+++ axwx/get_wu_data.py
@@ -1,27 +1,33 @@
 from axwx import wu_metadata_scraping as wumeta
 from axwx import wu_observation_scraping as wuobs
 
-# get station IDs from station_data.csv and subset by lat/lon bounds
-station_data_csv = "../data/station_data.csv"
-lat_range = [47.4, 47.8]
-lon_range = [-122.5, -122.2]
-all_station_ids_in_box = wumeta.get_station_ids_by_coords(station_data_csv, lat_range, lon_range)
+def get_wu_obs(station_data_csv, startdate, enddate, data_dir, index_start=0,
+               index_end=-1, lat_range=[47.4, 47.8], lon_range=[-122.5, -122.2]):
+    """
+    Pull PWS observations from WU
+    :param station_data_csv: str
+        station data csv filepath
+    :param startdate int
+        start date for data to retrieve (YYYYMMDD)
+    :param enddate int
+        end date for data to retrieve (YYYYMMDD)
+    :param data_dir str
+        data directory for new files
+    :param index_start: int
+        index start for station list
+    :param index_end: int
+        index end for station list
+    :return: None
+    """
+    # get station IDs from station_data.csv and subset by lat/lon bounds
 
-# subset station id list to reduce length of pull
-station_ids = all_station_ids_in_box[300:]
+    all_station_ids_in_box = wumeta.get_station_ids_by_coords(station_data_csv,
+                                                              lat_range, lon_range)
 
-print("Attempting to pull data for the following stations:")
-print(station_ids)
+    # subset station id list to reduce length of pull
+    station_ids = all_station_ids_in_box[index_start:index_end]
 
-data_dir = "/Users/Thompson/Desktop/DATA 515/" \
-           "Final Project/data/local/wu_station_data/full_period/"
+    print("Attempting to pull data for the following stations:")
+    print(station_ids)
 
-# wuobs.scrape_data_multiple_stations_and_days(station_ids, 20160430, 20170430, data_dir)
-
-# filepath = print(data_dir + "IWAKIRKL2.p")
-# print(filepath)
-
-# import pickle
-# IWAKIRKL2 = pickle.load(open("/Users/Thompson/Desktop/DATA 515/Final Project/"
-#                              "data/local/wu_station_data/full_period/IWAKIRKL2.p", "rb"))
-# print(IWAKIRKL2.shape)
+    wuobs.scrape_data_multiple_stations_and_days(station_ids, startdate, enddate, data_dir)
