6157ae35d1283174340d961451034b771e91788f
Dane Jordan <drjordan@uw.edu>
2017-06-06 04:09:00 -0700

    added data directory csv files to install

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ opts = dict(name='Ax/Wx',
             author_email='axwx@googlegroups.com',
             version='0.1',
             packages=PACKAGES,
-            package_data="PACKAGE_DATA",
+            package_data={'axwx': ['data/*.csv']},
             # install_requires="REQUIRES",
             # requires="REQUIRES"
             )
